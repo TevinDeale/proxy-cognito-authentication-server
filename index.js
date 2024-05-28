8bd4d0670861f5ed73f83e37d72ce93188cfd95f
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-05-27 20:09:58 -0500

    updated cors settings

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [FRONT_END_URL, PROD_FRONT_END_URL],
+    origin: [FRONT_END_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
